8923366579989f9593cc29d96370f940181ea1d4
= <=>
2018-09-14 10:11:14 +0700

    Add new file

---
+++ resources/views/templates/master.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<!-- Mirrored from www.uxithemes.com/html/turbino/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 12 Sep 2018 09:58:44 GMT -->
+<head>
+        <title>Home - Turbino Template</title>
+        <base href="{{asset('')}}">
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="source/assets/images/favicon.png">
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Seo meta tags for Search Engine Optimization -->
+        <meta name="author" content="Turbino Template">
+        <meta name="description" content="Travel agency Template for all types of tour agencies, travel bloggers, boostrap 4 based Template">
+        <meta name="keywords" content="Turbino, Turbino, travel agency, Templateforest tour agency, travel, adventure, destinations,">
+
+        <!-- Bootstrap and Jquery scripts -->
+        <script src="source/js/jquery-3.1.1.min.js"></script>
+        <script src="source/js/bootstrap.min.js"></script>
+        <script src="source/js/popper.min.js"></script>
+
+        <!-- Aditional scripts -->
+        <script src="source/js/jquery.magnific-popup.min.js"></script>
+        <script src="source/js/smooth-scroll.min.js"></script>
+        <script src="source/js/instafeed.min.js"></script>
+        <script src="source/js/ofi.js"></script>
+
+        <!-- Main scripts -->
+        <script src="source/js/main.js"></script>
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="source/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../../use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+        <link rel="stylesheet" href="source/css/font-awesome-animation.min.css">
+        <link rel="stylesheet" href="source/css/magnific-popup.css">
+        <link rel="stylesheet" href="source/css/style.css" type="text/css">
+    </head>
+
+   <body class="home1">
+    <!-- navbar -->
+    
+           @include('templates.header')       
+           @yield('content')
+
+        <!-- Footer -->
+            @include('templates.footer')
+        
+        <!-- End of Footer -->
+                            
+    </body>
+
+<!-- Mirrored from www.uxithemes.com/html/turbino/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 12 Sep 2018 09:58:58 GMT -->
+</html> 
