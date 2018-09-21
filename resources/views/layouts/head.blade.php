865b6274e823c6de36d546381ce4ba12db36d63b
datchau2310 <datchau23101994@gmail.com>
2018-09-22 02:19:42 +0700

    update giao dien v.2

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,35 @@
+<head>
+    <title>Home - Turbino Template</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Seo meta tags for Search Engine Optimization -->
+    <meta name="author" content="Turbino Template">
+    <meta name="description" content="Travel agency Template for all types of tour agencies, travel bloggers, boostrap 4 based Template">
+    <meta name="keywords" content="Turbino, Turbino, travel agency, Templateforest tour agency, travel, adventure, destinations,">
+
+    <!-- Bootstrap and Jquery scripts -->
+    <script src="{{ asset('/page/js/jquery-3.1.1.min.js') }}"></script>
+    <script src="{{ asset('/page/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/page/js/popper.min.js') }}"></script>
+
+    <!-- Aditional scripts -->
+    <script src="{{ asset('/page/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('/page/js/smooth-scroll.min.js') }}"></script>
+    <script src="{{ asset('/page/js/instafeed.min.js') }}"></script>
+    <script src="{{ asset('/page/js/ofi.js') }}"></script>
+
+    <!-- Main scripts -->
+    <script src="{{ asset('/page/js/main.js') }}"></script>
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('/page/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/page/use.fontawesome.com/releases/v5.0.13/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('/page/css/font-awesome-animation.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/page/css/magnific-popup.css') }}">
+    <link rel="stylesheet" href="{{ asset('/page/css/style.css') }}" type="text/css">
+</head>
